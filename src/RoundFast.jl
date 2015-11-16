cfbdeb8d7b7b0fad3536eba4b5f1023f522fbfb5
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-15 19:22:24 -0500

    Update RoundFast.jl

--- src/RoundFast.jl
+++ src/RoundFast.jl
@@ -1,8 +1,10 @@
 module RoundFast
 
-include "adjacent.jl"
-include "eftArith.jl"
-include "eftRound.jl"
-include "roundFast.jl"
+import Base:(+),(-),(*),(/),sqrt
+
+include("adjacent.jl")
+include("eftArith.jl")
+include("eftRound.jl")
+include("roundFast.jl")
 
 end # module
