f73a5e93244c0949ecc37dfb8d209ea15a8beeee
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-16 02:05:36 -0500

    Update eftRound.jl

--- src/eftRound.jl
+++ src/eftRound.jl
@@ -39,16 +39,16 @@
 
 
 @inline eftRound{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:ToZero}) = 
-    (signbit(hi)!=signbit(lo) ? nextNearerToZero(hi) : hi)
+    (lo == zero(T) ? hi : (signbit(hi)!=signbit(lo) ? nextNearerToZero(hi) : hi))
 
 @inline eftRound{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:FromZero}) = 
-    (signbit(hi)==signbit(lo) ? nextAwayFromZero(hi) : hi)
+    (lo == zero(T) ? hi : (signbit(hi)==signbit(lo) ? nextAwayFromZero(hi) : hi))
 
 @inline eftRound{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Up}) = 
-    (signbit(lo) ? hi : nextFloat(hi))
+    (lo == zero(T) ? hi : (signbit(lo) ? hi : nextFloat(hi)))
 
 @inline eftRound{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Down}) = 
-    (signbit(lo) ? prevFloat(hi) : hi)
+    (lo == zero(T) ? (signbit(lo) ? prevFloat(hi) : hi))
 
 @inline eftRound{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Nearest}) = 
     (hi)
